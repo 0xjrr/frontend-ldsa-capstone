b8dcbc62b15c178c252e5b92762168f059d68b6d
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-05-12 13:42:19 +0100

    add app elements and Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <ul>
+        <li>
+          <h1>LDSA Capstone form app</h1>
+        </li>
+        <li className='elem-github-logo'>
+          <a href="https://github.com/0xjrr/">
+            <img className='github-logo' src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="logo" />
+          </a>
+        </li>
+      </ul>
+
+      <div className="App">
+        <Form />
+      </div>
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ricardo Ribeiro
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </footer>
+    </>
   );
 }
 
